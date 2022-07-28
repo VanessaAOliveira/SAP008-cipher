1f2ccb9741cd303bc7110d370e74733f4ca8ba14
Vanessa Oliveira <oliveira.ameni@gmail.com>
2022-07-28 15:26:24 -0300

    finalizer cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,36 @@
+
 const cipher = {
-  // ...
+  encode(offset, word) {
+    if (typeof word !== 'string') {
+      throw new TypeError('Digite apenas letras');
+    }
+    let codedWord = '';
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < word.length; i++) {
+      const lettersUppercase = word.toUpperCase();
+      const convert = (lettersUppercase.charCodeAt(i) + offset - 65) % 26;
+      const conversionResult = convert + 65;
+      const result = String.fromCharCode(conversionResult);
+      codedWord += result;
+    }
+    return codedWord;
+  },
+  decode(offset, word) {
+    if (typeof word !== 'string') {
+      throw new TypeError('Digite apenas letras');
+    }
+    let codedWord = '';
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < word.length; i++) {
+      const lettersUppercase = word.toUpperCase();
+      const convert = (lettersUppercase.charCodeAt(i) - offset + 65) % 26;
+      const conversionResult = convert + 65;
+      const result = String.fromCharCode(conversionResult);
+      codedWord += result;
+    }
+    return codedWord;
+  },
+
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const buttonCode = document.getElementById('buttonEncode');
+buttonCode.addEventListener('click', encodeWord);
+
+function encodeWord() {
+  const word = document.getElementById('inputText').value;
+  const offset = parseInt(document.getElementById('inputOffset').value);
+  document.getElementById('inputConvertedText').value = cipher.encode(offset, word);
+}
+
+const buttonDecod = document.getElementById('buttonDecode');
+buttonDecod.addEventListener('click', decodeWord);
+
+function decodeWord() {
+  const word = document.getElementById('inputText').value;
+  const offset = parseInt(document.getElementById('inputOffset').value);
+  document.getElementById('inputConvertedText').value = cipher.decode(offset, word);
+}
+
+const buttonClean = document.getElementById('buttonReset');
+buttonClean.addEventListener('click', reset);
+
+function reset() {
+  document.getElementById('myForm').reset();
+}
+
+const buttonClean2 = document.getElementById('buttonReset');
+buttonClean2.addEventListener('click', reset2);
+
+function reset2() {
+  document.getElementById('myForm').reset();
+}
